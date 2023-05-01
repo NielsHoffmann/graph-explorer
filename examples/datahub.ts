707d1cf70e926a738858354186f2060fb4f30341
Niels <niels.hoffmann@hotmail.com>
2023-05-01 17:14:19 +0200

    fix build errors

--- examples/datahub.ts
+++ examples/datahub.ts
@@ -2,30 +2,33 @@ import { createElement, ClassAttributes } from 'react';
 import * as ReactDOM from 'react-dom';
 
 import {
-    Workspace, WorkspaceProps, SparqlDataProvider, SparqlQueryMethod
+    Workspace, WorkspaceProps, SparqlDataProvider, SparqlQueryMethod, OWLRDFSSettings
 } from '../src/graph-explorer/index';
 
 import { onPageLoad, tryLoadLayoutFromLocalStorage, saveLayoutToLocalStorage } from './common';
 
-const ConfigData: any = require('../auth_config.json')
+// const ConfigData: any = require('../auth_config.json')
         
 
 function onWorkspaceMounted(workspace: Workspace) {
     if (!workspace) { return; }
 
     const diagram = tryLoadLayoutFromLocalStorage();
+    let SparqlSettings = OWLRDFSSettings
+
     workspace.getModel().importLayout({
         diagram,
         validateLinks: true,
         dataProvider: new SparqlDataProvider({
             //endpointUrl: 'http://10.0.0.119:7200/repositories/datahub?infer=false',
-            endpointUrl: 'https://api.data.noord-holland.nl/graphdb/datahub?infer=false',
+            endpointUrl: 'http://localhost:3030/dataService/sparql',
             queryMethod: SparqlQueryMethod.GET,
-            acceptBlankNodes: true,
+            //acceptBlankNodes: true,
             //NH 30-7-2021 - custom header implementation
-            headers: ConfigData
+            // headers: ConfigData,
             //queryFunction: qf
-        }, ),
+        }, //SparqlSettings 
+        ),
     });
 }
 
@@ -45,4 +48,6 @@ const props: WorkspaceProps & ClassAttributes<Workspace> = {
     language: 'nl'
 };
 
-onPageLoad(container => ReactDOM.render(createElement(Workspace, props), container));
+onPageLoad((container) => {
+    ReactDOM.render(createElement(Workspace, props), container);
+});
